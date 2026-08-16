69e463a7a5ce33e4ed18a49cc051c3a0f7ca7075
Nadine <nadine@Nadines-MacBook-Pro.local>
2026-08-16 18:04:31 -0400

    fix: subsidy mechanism

--- core/mechanism/algorithms/subsidy.py
+++ core/mechanism/algorithms/subsidy.py
@@ -9,10 +9,14 @@ from core.types import MultiAgentDict
 @dataclass(frozen=True)
 class SubsidyMechanism(Mechanism):
     subsidy: float
+    cost: float
     action_component: int = 1
+
+    # default_cost = 
 
     def __post_init__(self) -> None:
         assert 0.0 <= self.subsidy <= 0.5
+        assert 0.0 <= self.cost <= 1.0 # TODO
 
     def to_vector(self) -> np.ndarray:
         return np.array([self.subsidy / 0.5], dtype=np.float32)
@@ -25,8 +29,14 @@ class SubsidyMechanism(Mechanism):
         rewards: MultiAgentDict,
         **kwargs,
     ) -> MultiAgentDict:
-        actions = kwargs["action_after"]
+        actions = kwargs["action_after"] # TODO fix this, passing action after and before
+
         return {
-            agent_id: reward + self.subsidy * actions[agent_id][self.action_component]
+            agent_id: 
+            reward 
+            + self.subsidy 
+            * actions[agent_id][self.action_component]
+            - self.cost 
+            * actions[agent_id[self.action_component]] ** 2
             for agent_id, reward in rewards.items()
         }
